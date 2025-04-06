22c343ac7607267459d2dad1eeeaa9e2fa8b51d0
masamallow <77992722+masamallow@users.noreply.github.com>
2025-04-06 18:53:37 +0900

    Resolve srcDir config and references

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-
   manifest: {
-    manifest_version: 3,
     name: 'Infinite Scroll Breaker',
     version: '0.1.0',
     description: 'Stops infinite scrolling on YouTube Shorts',
-    srcDir: 'src',
   },
+  srcDir: "src",
 });
