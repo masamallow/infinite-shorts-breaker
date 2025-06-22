5e68fa71e97f7313b47dcd8f1f7303f9bd6d7ec1
masamallow <77992722+masamallow@users.noreply.github.com>
2025-06-22 17:15:06 +0900

    refactor: Update extension name to include features to showcase extension features in search listings

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,9 @@ import {defineConfig} from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: 'Infinite Shorts Breaker',
+    name: 'Infinite Shorts Breaker: Set viewing and time limits!',
     version: '0.1.0',
-    description: 'Stop infinite scrolling on YouTube Shorts',
+    description: 'Stop infinite scrolling on YouTube Shorts with configurable limits.',
     permissions: ['storage'],
     content_security_policy: {
       extension_pages: "script-src 'self'; object-src 'self';",
